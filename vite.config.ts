3660d5afb05051aa92ca50dee4eb8e6897808987
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2024-02-23 09:35:23 +0000

    Remove PWA stuff that didn't work

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,6 @@
 import { defineConfig } from "vite";
 import voby from "voby-vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [
-    voby(),
-    VitePWA({
-      strategies: "injectManifest",
-      registerType: "autoUpdate",
-      injectRegister: "inline",
-      srcDir: "./",
-      filename: "sw.js",
-      manifest: {
-        name: "My Voby App",
-        short_name: "Voby App",
-        description: "My Super Fast Voby App",
-        theme_color: "#FFF6DB",
-        start_url: "/?source=pwa",
-        icons: [
-          {
-            src: "/pwa-logos/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-logos/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-logos/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [voby()],
 });
