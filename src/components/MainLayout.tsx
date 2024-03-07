efad81e9ec9832f411c147a444ce12d940e0841d
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2024-03-07 12:45:49 +0000

    Fix settings keys display in main layout

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,20 +1,14 @@
-import { $ } from "voby"
 import "./MainLayout.css"
 import { App } from "../app.mjs"
-import { promiseValue } from "../utilities.mjs"
 
 function MainLayout({ app }: { app: App }): JSX.Element {
-  const settingsKeys = promiseValue(
-    app.settings.getKeys().then((keys) => keys.join(", "))
-  )
-
   return (
     <div class="main-layout">
       <main>
         <div class="get-started">
           <p>Get started by opening a file</p>
           <p>Note: File opening has not been implemented yet</p>
-          <p>Settings: {settingsKeys}</p>
+          <p>Settings: {app.settings.getKeys()}</p>
         </div>
       </main>
     </div>
