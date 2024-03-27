25b412952617d08bcc2d2a46e11e9b4fe84fd3a9
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2024-03-27 12:10:21 +0000

    Add beercss

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,11 @@
 import { render } from "voby"
 
 import "./global.css"
+import "beercss/dist/cdn/beer.min.css"
 import MainLayout from "./components/MainLayout"
 import { App } from "./app.mjs"
+import "beercss"
+import "material-dynamic-colors"
 
 declare global {
   interface Window {
@@ -13,5 +16,15 @@ declare global {
 
 const app = new App()
 await app.init()
+
+function updateTheme(useDarkMode: boolean) {
+  const theme_color = "#C1694F"
+  ui("mode", useDarkMode ? "dark" : "light")
+  ui("theme", theme_color)
+}
+
+const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)")
+updateTheme(darkModeQuery.matches)
+darkModeQuery.addEventListener("change", (e) => updateTheme(e.matches))
 
 render(<MainLayout app={app} />, document.getElementById("app"))
