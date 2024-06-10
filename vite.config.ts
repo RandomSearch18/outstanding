6513d1707af7de9191ae10246bacdea96dc235c7
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2024-06-10 10:37:27 +0000

    Tell Vite that we're only supporting browsers with top-level await

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import { defineConfig } from "vite";
-import voby from "voby-vite";
+import { defineConfig } from "vite"
+import voby from "voby-vite"
 
 export default defineConfig({
   plugins: [voby()],
-});
+  esbuild: {
+    supported: {
+      "top-level-await": true,
+    },
+  },
+})
